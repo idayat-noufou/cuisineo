a3475b0cf3c549d65534e24ee332fe884f075b0e
Nwlcnf <153742803+Nwlcnf@users.noreply.github.com>
2024-07-02 10:10:12 +0200

    Correction pour get et post recette

--- back/src/main/java/com/ynov/webfullstack/back/Service/RecetteService.java
+++ back/src/main/java/com/ynov/webfullstack/back/Service/RecetteService.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Service
 public class RecetteService {
 
+    @Autowired
     private RecetteRepository recetteRepository;
 
     public Optional<Recette> getRecette(Long id) {
